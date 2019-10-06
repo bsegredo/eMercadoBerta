39a3af2b44d940b912a243dbba59c0c513000d21
bsegredo <54675460+bsegredo@users.noreply.github.com>
2019-10-06 10:58:36 -0300

    Segunda entrega
    Contiene el login, pero no funciona el botón de usuario. No trae la información del login, da error en el init.js.

---
+++ ecommerce-master/js/init.js
@@ -0,0 +1,54 @@
+const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
+const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
+const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
+const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
+
+function save(){
+  sessionStorage.setItem("name", document.getElementById("inputName").value);
+}
+
+function show(){
+  var user = sessionStorage.getItem("name");
+  if (user !=null){
+    document.getElementById("userName").innerHTML = user;
+  }else{
+    document.getElementById("userName").innerHTML = "Invitado";
+  }
+
+}
+
+var showSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "block";
+}
+
+var hideSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "none";
+}
+
+var getJSONData = function(url){
+    var result = {};
+    showSpinner();
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          hideSpinner();
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner();
+        return result;
+    });
+}
